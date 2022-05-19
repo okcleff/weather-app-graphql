8441e1ea17eac8510e59a6c659d814e7f58439e7
woncheolok <okcleff@gmail.com>
2022-05-19 23:05:37 +0900

    도시명, 기온, 설명, 풍속 렌더링; 스타일 미적용

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLazyQuery } from '@apollo/client';
 import { GET_WEATHER_QUERY } from '../graphql/Queries';
 
 const Home = () => {
-  const [getWeather, { loading, data, error }] = useLazyQuery(
-    GET_WEATHER_QUERY,
-    {
-      variables: { name: 'Seoul' },
-    }
-  );
+  const [citySearched, setCitySearched] = useState();
+  const [getWeather, { data, error }] = useLazyQuery(GET_WEATHER_QUERY, {
+    variables: { name: citySearched },
+  });
 
   if (error) return <h1>Error found</h1>;
   if (data) {
     console.log(data);
   }
 
+  const searchCity = (e) => {
+    e.preventDefault();
+    getWeather();
+  };
+
   return (
     <div className='home'>
       <h1>Search For Weather</h1>
-      <input type='text' placeholder='City name...' />
-      <button onClick={() => getWeather()}>Search</button>
+      <form>
+        <input
+          type='text'
+          placeholder='City name...'
+          onChange={(e) => setCitySearched(e.target.value)}
+        />
+        <button onClick={searchCity} type='submit'>
+          Search
+        </button>
+      </form>
+      <div className='weather'>
+        {data && (
+          <>
+            <h1> {data.getCityByName.name} </h1>
+            <h1>
+              Temperature: {data.getCityByName.weather.temperature.actual}
+            </h1>
+            <h1>
+              Description: {data.getCityByName.weather.summary.description}
+            </h1>
+            <h1>Wind Speed: {data.getCityByName.weather.wind.speed}</h1>
+          </>
+        )}
+      </div>
     </div>
   );
 };
